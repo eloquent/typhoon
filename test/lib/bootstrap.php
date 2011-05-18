e2355fcdbfad83d1b572c41b4228847451480022
Erin Millard <emwebdev@gmail.com>
2011-05-19 09:56:12 +1000

    fixed test suite pathing issues
    * PHPUnit XML can't handle multiple include path additions in a cross-platform manner, moved to bootstrap.php
    * fixed broken report cleaning
    * test suite should now run cross-platform, paving the way for CI server integration

--- test/lib/bootstrap.php
+++ test/lib/bootstrap.php
@@ -11,9 +11,23 @@
 
 error_reporting(E_ALL | E_STRICT | E_DEPRECATED);
 
-if (!defined('TYPHOON_TEST_COVERAGE_DIR'))
+if (!defined('TYPHOON_ROOT_DIR')) define('TYPHOON_ROOT_DIR', dirname(dirname(__DIR__)));
+if (!defined('TYPHOON_DEPLOY_DIR')) define('TYPHOON_DEPLOY_DIR', TYPHOON_ROOT_DIR.DIRECTORY_SEPARATOR.'deploy');
+if (!defined('TYPHOON_LIB_DIR')) define('TYPHOON_LIB_DIR', TYPHOON_DEPLOY_DIR.DIRECTORY_SEPARATOR.'lib');
+if (!defined('TYPHOON_TEST_DIR')) define('TYPHOON_TEST_DIR', TYPHOON_ROOT_DIR.DIRECTORY_SEPARATOR.'test');
+if (!defined('TYPHOON_TEST_LIB_DIR')) define('TYPHOON_TEST_LIB_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'lib');
+if (!defined('TYPHOON_TEST_SUITE_DIR')) define('TYPHOON_TEST_SUITE_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'suite');
+if (!defined('TYPHOON_TEST_REPORT_DIR')) define('TYPHOON_TEST_REPORT_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'report');
+
+if (!defined('TYPHOON_INCLUDE_PATH_SET'))
 {
-  define('TYPHOON_TEST_COVERAGE_DIR', dirname(__DIR__).DIRECTORY_SEPARATOR.'coverage');
+  define('TYPHOON_INCLUDE_PATH_SET', true);
+
+  set_include_path(
+    get_include_path()
+    .PATH_SEPARATOR.TYPHOON_LIB_DIR
+    .PATH_SEPARATOR.TYPHOON_TEST_LIB_DIR
+  );
 }
 
 // spl_autoload default implementation SHOULD do this itself, but it does not work for me
@@ -22,9 +36,11 @@ spl_autoload_register(function($name)
   include str_replace('\\', DIRECTORY_SEPARATOR, strtolower($name)).'.php';
 });
 
-// clean coverage reports
-typhoon_test_delete_recursive(TYPHOON_TEST_COVERAGE_DIR.DIRECTORY_SEPARATOR.'report');
-typhoon_test_delete_recursive(TYPHOON_TEST_COVERAGE_DIR.DIRECTORY_SEPARATOR.'coverage.xml');
+// clean reports
+foreach(glob(TYPHOON_TEST_REPORT_DIR.DIRECTORY_SEPARATOR.'*') as $report)
+{
+  typhoon_test_delete_recursive($report);
+}
 
 function typhoon_test_delete_recursive($path)
 {
