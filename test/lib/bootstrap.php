a93e28cc995af366f8ce200cd8ed15bee0176004
Erin Millard <emwebdev@gmail.com>
2011-08-10 00:28:14 +1000

    Revert "Merge branch 'master' of github.com:ezzatron/typhoon"
    This reverts commit daccaf71ff9af8f31df3cdaa2fa1ce76326c477b, reversing
    changes made to 4548b6f401d9b8bfabfe69933f909595cfd1e68c.

--- test/lib/bootstrap.php
+++ test/lib/bootstrap.php
@@ -11,23 +11,9 @@
 
 error_reporting(E_ALL | E_STRICT | E_DEPRECATED);
 
-if (!defined('TYPHOON_ROOT_DIR')) define('TYPHOON_ROOT_DIR', dirname(dirname(__DIR__)));
-if (!defined('TYPHOON_DEPLOY_DIR')) define('TYPHOON_DEPLOY_DIR', TYPHOON_ROOT_DIR.DIRECTORY_SEPARATOR.'deploy');
-if (!defined('TYPHOON_LIB_DIR')) define('TYPHOON_LIB_DIR', TYPHOON_DEPLOY_DIR.DIRECTORY_SEPARATOR.'lib');
-if (!defined('TYPHOON_TEST_DIR')) define('TYPHOON_TEST_DIR', TYPHOON_ROOT_DIR.DIRECTORY_SEPARATOR.'test');
-if (!defined('TYPHOON_TEST_LIB_DIR')) define('TYPHOON_TEST_LIB_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'lib');
-if (!defined('TYPHOON_TEST_SUITE_DIR')) define('TYPHOON_TEST_SUITE_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'suite');
-if (!defined('TYPHOON_TEST_REPORT_DIR')) define('TYPHOON_TEST_REPORT_DIR', TYPHOON_TEST_DIR.DIRECTORY_SEPARATOR.'report');
-
-if (!defined('TYPHOON_INCLUDE_PATH_SET'))
+if (!defined('TYPHOON_TEST_COVERAGE_DIR'))
 {
-  define('TYPHOON_INCLUDE_PATH_SET', true);
-
-  set_include_path(
-    get_include_path()
-    .PATH_SEPARATOR.TYPHOON_LIB_DIR
-    .PATH_SEPARATOR.TYPHOON_TEST_LIB_DIR
-  );
+  define('TYPHOON_TEST_COVERAGE_DIR', dirname(__DIR__).DIRECTORY_SEPARATOR.'coverage');
 }
 
 // spl_autoload default implementation SHOULD do this itself, but it does not work for me
@@ -36,11 +22,9 @@ spl_autoload_register(function($name)
   include str_replace('\\', DIRECTORY_SEPARATOR, strtolower($name)).'.php';
 });
 
-// clean reports
-foreach(glob(TYPHOON_TEST_REPORT_DIR.DIRECTORY_SEPARATOR.'*') as $report)
-{
-  typhoon_test_delete_recursive($report);
-}
+// clean coverage reports
+typhoon_test_delete_recursive(TYPHOON_TEST_COVERAGE_DIR.DIRECTORY_SEPARATOR.'report');
+typhoon_test_delete_recursive(TYPHOON_TEST_COVERAGE_DIR.DIRECTORY_SEPARATOR.'coverage.xml');
 
 // include Phake for improved mocking support
 if (!defined('TYPHOON_PHAKE_SRC_DIR'))
