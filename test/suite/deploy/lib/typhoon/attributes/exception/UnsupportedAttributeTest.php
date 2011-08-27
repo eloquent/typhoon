766eb35944ae1bb83120aabf11662b76c7d51f3b
Erin Millard <emwebdev@gmail.com>
2011-08-27 19:04:43 +1000

    implemented generic attributes class

---
+++ test/suite/deploy/lib/typhoon/attributes/exception/UnsupportedAttributeTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Typhoon package.
+ *
+ * Copyright © 2011 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Typhoon\Attributes\Exception;
+
+use Typhoon\Primitive\String;
+use Typhoon\Test\ExceptionTestCase;
+
+class UnsupportedAttributeTest extends ExceptionTestCase
+{
+  /**
+   * @return string
+   */
+  protected function exceptionClass()
+  {
+    return __NAMESPACE__.'\UnsupportedAttribute';
+  }
+
+  /**
+   * @return array
+   */
+  protected function defaultArguments()
+  {
+    return array($this->_className, $this->_attribute);
+  }
+
+  protected function setUp()
+  {
+    parent::setUp();
+
+    $this->_className = new String('foo');
+    $this->_attribute = new String('bar');
+  }
+
+  /**
+   * @covers Typhoon\Attributes\Exception\UnsupportedAttribute::__construct
+   */
+  public function testConstructor()
+  {
+    $this->assertEquals("The attribute '".$this->_attribute."' is not supported by class '".$this->_className."'.", $this->exceptionFixture()->getMessage());
+
+    parent::testConstructor();
+  }
+
+  /**
+   * @var String
+   */
+  protected $_className;
+
+  /**
+   * @var String
+   */
+  protected $_attribute;
+}
