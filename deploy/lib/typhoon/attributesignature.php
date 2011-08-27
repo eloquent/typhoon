1af3e045bb2a31d9a98c070da7fef033e1d9e2cb
Erin Millard <emwebdev@gmail.com>
2011-08-27 18:11:04 +1000

    added attribute signature class for defining available attributes

---
+++ deploy/lib/typhoon/attributesignature.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Typhoon package.
+ *
+ * Copyright © 2011 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Typhoon;
+
+use Typhoon\Type\Object as ObjectType;
+use Typhoon\Type\String as StringType;
+
+class AttributeSignature extends Collection
+{
+  /**
+   * @return Type
+   */
+  protected function keyType()
+  {
+    return new StringType;
+  }
+
+  /**
+   * @return Type
+   */
+  protected function valueType()
+  {
+    $type = new ObjectType;
+    $type->setTyphoonAttribute(ObjectType::ATTRIBUTE_CLASS, 'Typhoon\Type');
+
+    return $type;
+  }
+
+  /**
+   * @return boolean
+   */
+  protected function allowEmptyKey()
+  {
+    return false;
+  }
+}
---
+++ test/suite/deploy/lib/typhoon/AttributeSignatureTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Typhoon package.
+ *
+ * Copyright © 2011 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Typhoon;
+
+use Phake;
+use Typhoon\Test\TestCase;
+
+class AttributeSignatureTest extends TestCase
+{
+  /**
+   * @return array
+   */
+  public function typeFailureData()
+  {
+    $type = Phake::mock(__NAMESPACE__.'\Type');
+
+    return array(
+      array('offsetSet', 'foo', 'bar'),
+      array('offsetSet', 'foo', 1),
+      array('offsetSet', 'foo', null),
+      array('offsetSet', 'foo', true),
+      array('offsetSet', 'foo', false),
+
+      array('offsetSet', 1, $type),
+      array('offsetSet', null, $type),
+      array('offsetSet', true, $type),
+      array('offsetSet', .1, $type),
+    );
+  }
+
+  protected function setUp()
+  {
+    $this->_signature = new AttributeSignature;
+  }
+
+  /**
+   * @covers Typhoon\AttributeSignature
+   */
+  public function testSignature()
+  {
+    $type = Phake::mock(__NAMESPACE__.'\Type');
+    $this->_signature['foo'] = $type;
+
+    $this->assertEquals($type, $this->_signature['foo']);
+  }
+
+  /**
+   * @covers Typhoon\AttributeSignature
+   * @dataProvider typeFailureData
+   */
+  public function testTypeFailures($method)
+  {
+    $arguments = func_get_args();
+    array_shift($arguments);
+
+    $this->setExpectedException('Typhoon\Assertion\Exception\UnexpectedType');
+    call_user_func_array(array($this->_signature, $method), $arguments);
+  }
+  
+  /**
+   * @var AttributeSignature
+   */
+  protected $_signature;
+}
