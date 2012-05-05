600394c5e404b36c75d22ce5cda13f737c84a790
Erin Millard <emwebdev@gmail.com>
2012-05-05 18:46:25 +1000

    Removed all calls that utilise Typhoon as a singleton. Relates to #22.

--- test/suite/src/Eloquent/Typhoon/TyphoonTest.php
+++ test/suite/src/Eloquent/Typhoon/TyphoonTest.php
@@ -21,9 +21,6 @@ class TyphoonTest extends \Eloquent\Typhoon\Test\TestCase
   protected function setUp()
   {
     $this->_typhoon = new Typhoon;
-    $this->_typeInspector = new TypeInspector;
-    $this->_typeRegistry = new TypeRegistry;
-    $this->_typeRenderer = Phake::mock('Eloquent\Typhoon\Type\Renderer\TypeRenderer');
   }
 
   /**
@@ -48,64 +45,7 @@ class TyphoonTest extends \Eloquent\Typhoon\Test\TestCase
   }
 
   /**
-   * @covers Eloquent\Typhoon\Typhoon::setTypeInspector
-   * @covers Eloquent\Typhoon\Typhoon::typeInspector
-   * @group core
-   */
-  public function testTypeInspector()
-  {
-    $this->assertInstanceOf('Eloquent\Typhoon\Type\Inspector\TypeInspector', $this->_typhoon->typeInspector());
-
-    $this->_typhoon->setTypeInspector($this->_typeInspector);
-
-    $this->assertSame($this->_typeInspector, $this->_typhoon->typeInspector());
-  }
-
-  /**
-   * @covers Eloquent\Typhoon\Typhoon::setTypeRegistry
-   * @covers Eloquent\Typhoon\Typhoon::typeRegistry
-   * @group core
-   */
-  public function testTypeRegistry()
-  {
-    $this->assertInstanceOf('Eloquent\Typhoon\Type\Registry\TypeRegistry', $this->_typhoon->typeRegistry());
-
-    $this->_typhoon->setTypeRegistry($this->_typeRegistry);
-
-    $this->assertSame($this->_typeRegistry, $this->_typhoon->typeRegistry());
-  }
-
-  /**
-   * @covers Eloquent\Typhoon\Typhoon::setTypeRenderer
-   * @covers Eloquent\Typhoon\Typhoon::typeRenderer
-   * @group core
-   */
-  public function testTypeRenderer()
-  {
-    $this->assertInstanceOf('Eloquent\Typhoon\Typhax\TyphaxTypeRenderer', $this->_typhoon->typeRenderer());
-
-    $this->_typhoon->setTypeRenderer($this->_typeRenderer);
-
-    $this->assertSame($this->_typeRenderer, $this->_typhoon->typeRenderer());
-  }
-
-  /**
    * @var Typhoon
    */
   protected $_typhoon;
-
-  /**
-   * @var TypeInspector
-   */
-  protected $_typeInspector;
-
-  /**
-   * @var TypeRegistry
-   */
-  protected $_typeRegistry;
-
-  /**
-   * @var TypeRenderer
-   */
-  protected $_typeRenderer;
 }
