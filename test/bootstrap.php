96517c1c22c3e298beb7778036a2976373508f45
Erin Millard <ezzatron@gmail.com>
2012-12-03 00:10:01 +1000

    Tidied up autoload configuration. Closes #53.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,6 +3,7 @@
 use Typhoon\Typhoon;
 
 $autoloader = require __DIR__.'/../vendor/autoload.php';
+$autoloader->add('Typhoon', __DIR__.'/../src-deploy');
 $autoloader->add('Eloquent\Typhoon\TestCase', __DIR__.'/src');
 $autoloader->add('Eloquent\Typhoon\TestFixture', __DIR__.'/src');
 $autoloader->add('Typhoon\Validator\Eloquent\Typhoon\TestFixture', __DIR__.'/src');
